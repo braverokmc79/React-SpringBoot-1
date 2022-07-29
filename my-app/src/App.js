95fd15b9ae3af8e6d47710ca1319205143b98602
braverokmc79 <braverokmc79@gmail.com>
2022-07-29 18:02:16 +0900

    2.실행흐름이해하기

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,13 @@
 import logo from './logo.svg';
 import "./App.css";
 
+//0.React 엔진 - 데이터변경감지에서UI 그려주는 !
+//1.실행과정(index.html) SPA 
+//2.JSX문법
+//3.바벨(자바스크립트 ES5)->ES6
 function App() {
-  return <div>안녕</div>;
+  return <div>안녕11333</div>;
 }
+
 
 export default App;
