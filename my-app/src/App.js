7290a7a2f29550a3244f1605e3cd34c0a224810a
braverokmc79 <braverokmc79@gmail.com>
2022-08-04 16:52:12 +0900

    6.useState

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
-
+import { useState } from "react";
 import "./App.css";
-import Wood from './wood';
+
 
 //0.React 엔진 - 데이터변경감지에서UI 그려주는 !
 //1.실행과정(index.html) SPA 
@@ -17,25 +16,51 @@ import Wood from './wood';
 //- 외부 파일에 적는 방법
 //- 라이브러리 사용 (부트스트랩, component-sytled)
 
-
-
 function App() {
-  let list = [1, 2, 3];
 
+  const [num, setNum] = useState(5);
+  console.log("App 실행됨")
+  let sample = [
+    { id: 1, name: "홍길동" },
+    { id: 2, name: "임꺽정" },
+    { id: 3, name: "장보고" },
+    { id: 4, name: "코스" },
+  ];
+
+  //다운로그 받음
+  const [users, setUsers] = useState(sample);// 레퍼런스가 변경되야 동작!!
+
+  const download = () => {
+
+    //기존 데이터 세팅
+    //const a = sample.concat({ id: 5, name: '조자룡' });
+    //console.log(a);
+    // setUsers([...sample]);
+    //setUsers(a);
+
+    setUsers([...sample, { id: num, name: "조자룡" }]);
+    setNum(num + 1);
+    setUsers.map(u => {
+      
+    });
+
+    //concat, filter, 전개연산자, map, slice
+  };
+
+  //랜더링 시점= 상태값 변경
   return (
     <div>
-      <div>
-        {list.map(function (n) {
-          return <h1>{n}</h1>;
-        })}
-      </div>
-    </div>
+      <button onClick={download}>다운로드</button>
+      {users.map(u =>
 
+        <h1>{u.id} : {u.name}</h1>
+      )}
+    </div >
   )
     ;
-
 
 }
 
 
 export default App;
+
---
+++ my-app/src/Sub.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Sub = () => {
+
+
+    //함수 제공!
+    return (
+        <div>
+            <h1>Sub입니다.</h1>
+        </div>
+    );
+
+
+
+
+};
+
+export default Sub;
+
+
+
